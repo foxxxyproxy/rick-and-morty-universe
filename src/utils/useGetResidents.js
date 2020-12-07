d36941088fe28ad31539c61b14d1e72e3a6d7016
Alexandra Ignatova <alex.ignatova@gmail.com>
2020-12-07 21:42:32 +0100

    refactor: add custom hook useGetResidents

---
+++ src/utils/useGetResidents.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function useGetResidents(residents) {
+  const [loading, setLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    if (!residents) return;
+    let dataArray = [];
+    setCharacters([]);
+
+    residents.forEach((resident) => {
+      setLoading(true);
+
+      fetch(resident)
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data) {
+            setLoading(false);
+          }
+          dataArray = [...dataArray, data];
+          setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+        })
+        .finally(() => {
+          setCharacters(dataArray);
+          setLoading(false);
+        });
+    });
+  }, [residents]);
+
+  return { characters, loading };
+}
